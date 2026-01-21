264e17714a4b52ceb127bcc94b14719454ce6f54
leeeeejieun <arjleun19@gmail.com>
2026-01-21 21:31:20 +0900

    2026-01-21 ansible.sh 파일 수정
    - storage1,2 서버를 대상으로 IP 기반 SSH 키 배포 및 접속 코드 추가

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,6 @@
+cd ~
+vi ansible.sh
+-------------------------------------------------------------------------------------------------------------------
 #!/bin/bash
 
 ### 변수 설정 ###
@@ -16,28 +19,37 @@ do
     ssh -o StrictHostKeyChecking=no devops@$host "exit"
 done
 
-# 3. ansible 패키지 설치
-# sudo yum -y install epel-release
-# sudo yum -y install ansible
+# 3. IP 대상 키 배포
+for ip in 10.5.4.90 10.5.4.100; do
+    sshpass -p "devops" ssh-copy-id -o StrictHostKeyChecking=no devops@$ip
+done
 
-# # 4. ande 설정
-# mkdir -p ~/bin
+# 4. IP 대상으로 한 번씩 접속
+for ip in 10.5.4.90 10.5.4.100; do
+    ssh -o StrictHostKeyChecking=no devops@$ip "exit"
+done
 
-# cat << 'EOF' > ~/bin/ande
-# #!/bin/bash
-# if [ $# -ne 1 ]; then
-#   echo "Usage: $0 <module name>"
-#   exit 1
-# fi
+# 5. ansible 패키지 설치
+sudo yum -y install epel-release
+sudo yum -y install ansible
 
-# ansible-doc "$1" 2>/dev/null | sed -n '/^[[:space:]]*EXAMPLES:/,$p' | more
+# 6. ande 설정
+mkdir -p ~/bin
 
-# EOF
+cat <<'EOF' > ~/bin/ande
+#!/bin/bash
 
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <module name>"
+  exit 1
+fi
 
-# chmod +x ~/bin/ande
+ANSIBLE_MODULE="$1"
+ansible-doc "$ANSIBLE_MODULE" | sed -n '/^EXAMPLES:/,$p' | more
+EOF
 
-# # .bashrc에 경로와 별칭 추가
-# grep -q "bin" ~/.bashrc || echo 'export PATH=$PATH:~/bin' >> ~/.bashrc
-# grep -q "alias anp=" ~/.bashrc || echo "alias anp='ansible-playbook'" >> ~/.bashrc
+chmod +x ~/bin/ande
 
+# 7. .bashrc에 경로와 별칭 추가
+grep -q "bin" ~/.bashrc || echo 'export PATH=$PATH:~/bin' >> ~/.bashrc
+grep -q "alias anp=" ~/.bashrc || echo "alias anp='ansible-playbook'" >> ~/.bashrc
